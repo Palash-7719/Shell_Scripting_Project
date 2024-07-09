5c5283cf7a3ced486f38922754f2a7252f3ef28a
Palash-7719 <110078878+Palash-7719@users.noreply.github.com>
2024-07-09 22:42:16 +0530

    Update list_read_users.sh

--- list_read_users.sh
+++ list_read_users.sh
@@ -57,3 +57,39 @@ function helper{
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
+
+
+#To connect and run on a vm
+#Create an EC2 instance logging into ur AWS Account
+#Coonect with the VM created using termninal
+ssh ubuntu@52.66.184.149
+#change the permission of the key pair
+chmod 600 /Users/Downloads/test76.pem
+#Now establish with the VM
+ssh -i /Users/Downloads/test76.pem ubuntu@52.66.184.149
+#Now the coonection is setup with the AWS EC2 instance(VM)
+#Now the clone with the github repo 
+git clone https://github.com/Palash-7719/Shell_Scripting_Project
+ls
+#Shell_Scripting_Project
+cd Shell_Scripting_Project
+#README.md #github-api
+cd github-api
+ls
+#list_read_users.sh
+#Now export username & token access
+export username="Palash-7719"
+export token="Token"
+./list_read_users.sh
+#opened the file and provide the command line arguments
+#Create a repo with ur organisation
+./list_read_users.sh Devops-example-77 demo-1
+#if permission denied
+chmod 777 list_read_users.sh
+./list_read_users.sh Devops-example-77 demo-1
+#It now list the users having read access within the organisation 
+#We can also change the permission for the other aspects such as admin,write etc using jq command as for a Json format file
+
+
+
+
